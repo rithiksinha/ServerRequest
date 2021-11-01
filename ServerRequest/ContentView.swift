0c7cf725fbce8bfd0187a62a660b330e6f2f35d5
rithiksinha <rithiksinha2000@gmail.com>
2021-11-01 16:37:36 +0530

    Initial Commit

---
+++ ServerRequest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ServerRequest
+//
+//  Created by Ritik Sinha on 01/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ServerRequest/ServerRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerRequestApp.swift
+//  ServerRequest
+//
+//  Created by Ritik Sinha on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ServerRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
